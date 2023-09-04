358fd276b53a3bdc66a7619f8a27bc7889885e67
Rashpinder Singh <rashpinder.singh@leanix.net>
2023-09-04 20:43:20 +0200

    Material App widget updated

---
+++ lib/materialApp/material_app.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MaterialAppWidget extends StatelessWidget {
+  const MaterialAppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Themed App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Scaffold(
+              appBar: AppBar(
+                title: const Text('My First Page'),
+              ),
+              body: const Center(
+                child: Text('Hello, World!'),
+              ),
+            ),
+        '/second': (context) => Scaffold(
+              appBar: AppBar(
+                title: const Text('My Second Page'),
+              ),
+              body: const Center(
+                child: Text('Hello, World!'),
+              ),
+            ),
+      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Material App'),
+        ),
+        body: const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
