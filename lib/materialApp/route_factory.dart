17241f2af8b23e5f91ce9353b19a745ca3357275
Rashpinder Singh <rashpinder.singh@leanix.net>
2023-09-05 07:13:27 +0200

    Added the route factory demo

---
+++ lib/materialApp/route_factory.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'On Generate Route Demo',
+      home: const HomePage(),
+      onGenerateRoute:
+          _onGenerateRoute, // Assign the _onGenerateRoute function to MaterialApp
+    );
+  }
+
+  Route<dynamic> _onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/second':
+        final String message = settings.arguments as String;
+        return MaterialPageRoute(
+          builder: (context) => SecondPage(message: message),
+        );
+      default:
+        return MaterialPageRoute(builder: (context) => const HomePage());
+    }
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to SecondPage using the onGenerateRoute
+            Navigator.pushNamed(
+              context,
+              '/second',
+              arguments: 'Hello from Home Page!',
+            );
+          },
+          child: const Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  final String message;
+
+  const SecondPage({
+    super.key,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Page')),
+      body: Center(
+        child: Text(message),
+      ),
+    );
+  }
+}
