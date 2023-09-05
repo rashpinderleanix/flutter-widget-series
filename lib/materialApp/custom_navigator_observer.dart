4321f5cf0ab94bc0117e46d6acc30d7ac373a357
Rashpinder Singh <rashpinder.singh@leanix.net>
2023-09-05 20:37:07 +0200

    Added the custom navigator observer

--- lib/materialApp/custom_navigator_observer.dart
+++ lib/materialApp/custom_navigator_observer.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
 class CustomNavigatorObserver extends NavigatorObserver {
-     
-     @override
+  @override
   void didPush(Route route, Route? previousRoute) {
     super.didPush(route, previousRoute);
     print('Navigated to ${route.settings.name}');
   }
+
   @override
   void didPop(Route route, Route? previousRoute) {
     print('Navigated back to ${previousRoute?.settings.name}');
     super.didPop(route, previousRoute);
   }
+
   @override
   void didRemove(Route route, Route? previousRoute) {
     print('Navigated remove to ${previousRoute?.settings.name}');
     super.didRemove(route, previousRoute);
   }
+
   @override
   void didReplace({Route? newRoute, Route? oldRoute}) {
     print('Navigation route replaced to ${newRoute?.settings.name}');
     super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
   }
-  
 }
